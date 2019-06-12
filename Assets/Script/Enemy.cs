d6a04fe7c2b247e525fba6321d1927a6e14f011b
chunleethong <chunlee.thong@gmail.com>
2019-06-12 20:35:25 +0700

    inital commit
    initial commit

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float Speed = 10f;
+	public int wavepointIndex = 0;
+	public Transform target;
+	void Start () {
+		target = Waypoints.points[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = target.position - transform.position;
+		transform.Translate(direction.normalized * Speed * Time.deltaTime,Space.World);
+		if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+		{
+			getNextWayPoints();
+		}
+
+	}
+	void getNextWayPoints()
+	{
+		if(wavepointIndex >= Waypoints.points.Length - 1)
+		{
+			Destroy(gameObject);
+			return;
+		}
+		wavepointIndex++;
+		target = Waypoints.points[wavepointIndex];
+
+	}
+}
