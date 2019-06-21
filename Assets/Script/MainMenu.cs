c9eebd2e6599db57ef576206cfc82f59469a8417
chunleethong <chunlee.thong@gmail.com>
2019-06-21 16:51:46 +0700

    add main menu

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public string LevelToLoad = "Level01";
+    public void Play()
+	{
+		SceneManager.LoadScene(LevelToLoad);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
