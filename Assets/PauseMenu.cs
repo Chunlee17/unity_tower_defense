f5d18da5b03abc37866dd40d83665d9159894b6b
chunleethong <chunlee.thong@gmail.com>
2019-06-21 13:22:43 +0700

    add pause menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+	public GameObject UI;
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Toggle();
+		}
+    }
+
+	void Toggle()
+	{
+		UI.SetActive(!UI.activeSelf);
+
+		if (UI.activeSelf == true) {
+			Time.timeScale = 0;
+		}
+		else
+		{
+			Time.timeScale = 1;
+		}
+		
+	}
+
+	public void Retry()
+	{
+		Toggle();
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void Menu()
+	{
+		Toggle();
+		Debug.Log("Go to Menu!!");
+	}
+
+	public void Continue()
+	{
+		Toggle();
+	}
+}
--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -19,7 +19,7 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetKey(KeyCode.Escape))
+		if (Input.GetKey(KeyCode.Space))
 			doMovement = !doMovement;
 		if (!doMovement)
 		{
