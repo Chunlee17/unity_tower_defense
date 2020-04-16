2946e5fb594bf63314a16a81fa96c52b5b7a3651
Chunlee Thong <44610456+Chunlee17@users.noreply.github.com>
2020-04-16 23:12:49 +0700

    asd

--- Assets/Script/Turret.cs
+++ Assets/Script/Turret.cs
@@ -103,11 +103,14 @@ public class Turret : MonoBehaviour
 
 	public void LockOnTarget()
 	{
-		Vector3 direction = target.position - transform.position;
 
-        Quaternion lookRotation = Quaternion.LookRotation(direction);
-        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
-        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+		partToRotate.LookAt(target);
+
+		//Vector3 direction = target.position - transform.position;
+
+  //      Quaternion lookRotation = Quaternion.LookRotation(direction);
+  //      Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+  //      partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
     }
     void updateTarget()
     {
